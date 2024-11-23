5ea94cd6c439cc376bb7071c9d343effbbe5fd9b
AlimightyYogi <nisaafriani957@gmail.com>
2024-11-23 15:09:58 +0700

    Update .env

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
     }
-);
+  );
 
 const connectDB = async () => {
     try {
